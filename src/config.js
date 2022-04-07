268855d8f06f519a89e88dab28de39a7360b33b3
azza1998 <62287515+azza1998@users.noreply.github.com>
2022-04-07 09:15:25 +0100

    config.js

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ config();
 
 const configurations = {
   PORT: process.env.PORT || 4000,
-  MONGODB_HOST: process.env.MONGODB_HOST || 172.30.6.44,
+  MONGODB_HOST: process.env.MONGODB_HOST || "mongo",
   MONGODB_DATABASE: process.env.MONGODB_DB || "notes-app",
-  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || 172.30.6.44}/${
+  MONGODB_URI: `mongodb://${process.env.MONGODB_HOST || "mongo"}/${
     process.env.MONGODB_DATABASE || "notes-app"
   }`,
 };
